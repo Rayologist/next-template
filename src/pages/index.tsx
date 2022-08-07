b5bf1dfc0107687de50a9a84a987c3a5e77ca941
Bo-wei Chen <rayologist1002@gmail.com>
2022-08-07 22:38:43 +0800

    Add color scheme toggle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import FormDemo from "@containers/Form";
-import { MediaQuery } from "@mantine/core";
+import { MediaQuery, Group } from "@mantine/core";
+import ColorSchemeToggle from "@components/ColorSchemeToggle";
 
 export default function HomePage() {
   return (
-    <MediaQuery smallerThan={700} styles={{ width: "70%" }}>
-      <FormDemo />
-    </MediaQuery>
+    <>
+      <Group position="right" mt={5}>
+        <ColorSchemeToggle />
+      </Group>
+      <MediaQuery smallerThan={700} styles={{ width: "50%" }}>
+        <FormDemo />
+      </MediaQuery>
+    </>
   );
 }
