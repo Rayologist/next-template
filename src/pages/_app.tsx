ac0f86cde1311bb629d89856658cc84a219647b9
Bo-wei Chen <rayologist1002@gmail.com>
2022-08-07 22:15:44 +0800

    Add `ColorSchemeProvider`, `MantineProvider`, and `NotificationsProvider`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,48 @@
 import type { AppProps } from "next/app";
+import { GetServerSidePropsContext } from "next";
+import { useState } from "react";
+import { getCookie, setCookie } from "cookies-next";
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+} from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme = value || colorScheme === "dark" ? "light" : "dark";
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <NotificationsProvider>
+          <Component {...pageProps} />
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
+
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
 
 export default App;
