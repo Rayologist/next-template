2a026662d59fc751947dbaf03442d0066e785208
Bo-wei Chen <rayologist1002@gmail.com>
2022-08-30 23:19:19 +0800

    Update icon

--- src/components/Form/FormikComponents/PasswordInput.tsx
+++ src/components/Form/FormikComponents/PasswordInput.tsx
@@ -1,7 +1,7 @@
 import { PasswordInput as MantinePasswordInput } from "@mantine/core";
 import { useCustomFormik } from "./Helper";
 import { PasswordInputProps } from "types";
-import { IconEyeCheck, IconEyeOff } from "@tabler/icons";
+import { IconEye, IconEyeOff } from "@tabler/icons";
 
 function PasswordInput(props: PasswordInputProps) {
   const { label, name, ...rest } = props;
@@ -17,7 +17,7 @@ function PasswordInput(props: PasswordInputProps) {
       onChange={formik.handleChange}
       onBlur={formik.handleBlur}
       visibilityToggleIcon={({ reveal, size }) =>
-        reveal ? <IconEyeOff size={size} /> : <IconEyeCheck size={size} />
+        reveal ? <IconEyeOff size={size} /> : <IconEye size={size} />
       }
       {...rest}
     />
