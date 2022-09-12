986cfdf098dacb2505de8aeb9831ba869a5a7e2e
Bo-wei Chen <rayologist1002@gmail.com>
2022-09-12 23:14:00 +0800

    Add table types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from 'react';
 import {
   PasswordInputProps as MantinePasswordInputProps,
   RadioGroupProps as MantineRadioGroupProps,
@@ -10,7 +11,65 @@ import {
   FileInputProps as MantineFileInputProps,
   ColProps,
 } from '@mantine/core';
-import { DatePickerProps as MantineDatePickerProps } from '@mantine/dates';
+import {
+  DatePickerProps as MantineDatePickerProps,
+  DateRangePickerProps as MantineDateRangePickerProps,
+  DateRangePickerValue,
+} from '@mantine/dates';
+import { RowData } from '@tanstack/react-table';
+import { inDateRange } from '@components/Table/components/ColumnFilter/FilterFn';
+import FilterInput from '@components/Table/components/ColumnFilter/FilterInput';
+
+declare module '@tanstack/react-table' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface ColumnMeta<TData extends RowData, TValue> {
+    filterInput?: FilterInput;
+  }
+  interface FilterFns {
+    inDateRange: typeof inDateRange;
+  }
+}
+
+export type InputFilterProps<T> = {
+  filterValue: T;
+  setFilterValue: Dispatch<SetStateAction<T>>;
+};
+
+export type OmitValueAndOnChange<T> = Omit<T, 'value' | 'onChange'>;
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export type TextInputFilter = OmitValueAndOnChange<MantineTextInputProps>;
+export type TextInputFilterProps = InputFilterProps<string> & TextInputFilter;
+
+export type SelectFilter = OmitValueAndOnChange<MantineSelectProps>;
+export type SelectFilterProps = InputFilterProps<string> & SelectFilter;
+
+export type DateRangePickerFilter = OmitValueAndOnChange<MantineDateRangePickerProps>;
+export type DateRangePickerFilterProps = InputFilterProps<DateRangePickerValue> &
+  DateRangePickerFilter;
+
+export type MultiSelectFilter = OmitValueAndOnChange<MantineMultiSelectProps>;
+export type MultiSelectFilterProps = InputFilterProps<string[]> & MultiSelectFilter;
+
+export type FilterInput =
+  | {
+      type: 'text';
+      props?: Partial<TextInputFilter>;
+    }
+  | {
+      type: 'select';
+      props?: Partial<SelectFilter>;
+    }
+  | {
+      type: 'date';
+      props?: Partial<DateRangePickerFilter>;
+    }
+  | {
+      type: 'multi-select';
+      props?: Partial<MultiSelectFilter>;
+    };
+
+export type FilterInputType = FilterInput['type'];
 
 export type Option = {
   label: string;
