86a03c804811ff6ec492825ea2a1fb5eb77a9dea
Bo-wei Chen <rayologist1002@gmail.com>
2022-12-29 19:54:27 +0800

    Update according to rhf-mantine template

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,13 +2,17 @@ import { Dispatch, SetStateAction, ReactNode } from 'react';
 import {
   PasswordInputProps as MantinePasswordInputProps,
   RadioGroupProps as MantineRadioGroupProps,
+  RadioProps,
   SelectProps as MantineSelectProps,
   TextareaProps as MantineTextareaProps,
   TextInputProps as MantineTextInputProps,
   CheckboxGroupProps as MantineCheckboxGroupProps,
+  CheckboxProps,
   NumberInputProps as MantineNumberInputProps,
   MultiSelectProps as MantineMultiSelectProps,
   FileInputProps as MantineFileInputProps,
+  SwitchGroupProps as MantineSwitchGroupProps,
+  SwitchProps,
   ColProps,
 } from '@mantine/core';
 import {
@@ -74,13 +78,13 @@ export type FilterInput =
 export type FilterInputType = FilterInput['type'];
 
 // ------React-Hook-Form-Controlled Mantine Component types------
-export type Option = {
+export type Option<OtherProps = {}> = {
   label: ReactNode;
   value: any;
-};
+} & OtherProps;
 
-export interface Options {
-  options: Option[];
+export interface Options<OtherProps = {}> {
+  options: Option<OtherProps>[];
 }
 
 export type Controlled<T> = { label: ReactNode; name: string } & T;
@@ -96,12 +100,19 @@ export type SelectProps = Controlled<
     options: MantineSelectProps['data'];
   }
 >;
-export type CheckboxGroupProps = Controlled<Omit<MantineCheckboxGroupProps, 'children'> & Options>;
-export type RadioGroupProps = Controlled<Omit<MantineRadioGroupProps, 'children'> & Options>;
 export type MultiSelectProps = Controlled<
   Omit<MantineMultiSelectProps, 'data'> & {
     options: MantineMultiSelectProps['data'];
   }
+>;
+export type CheckboxGroupProps = Controlled<
+  Omit<MantineCheckboxGroupProps, 'children'> & Options<CheckboxProps>
+>;
+export type RadioGroupProps = Controlled<
+  Omit<MantineRadioGroupProps, 'children'> & Options<RadioProps>
+>;
+export type SwitchGroupProps = Controlled<
+  Omit<MantineSwitchGroupProps, 'children'> & Options<SwitchProps>
 >;
 
 export type ControllerProps =
@@ -114,13 +125,18 @@ export type ControllerProps =
   | ({ control: 'date-picker' } & DatePickerProps)
   | ({ control: 'number-input' } & NumberInputProps)
   | ({ control: 'multi-select' } & MultiSelectProps)
-  | ({ control: 'file-input' } & FileInputProps<boolean>);
+  | ({ control: 'file-input' } & FileInputProps<boolean>)
+  | ({ control: 'switch-group' } & SwitchGroupProps);
+
+type Controllers<TFieldValues extends FieldValues, TContext> = {
+  [key in keyof TFieldValues]: ControllerProps & { name: key } & {
+    col?: ColProps;
+    after?: ReactNode | ((ctx: UseFormReturn<TFieldValues, TContext>) => ReactNode);
+  };
+};
 
 export type FormControllerProps<TFieldValues extends FieldValues = FieldValues, TContext = any> = {
-  controllers: {
-    [key in keyof TFieldValues]: ControllerProps & { name: key } & {
-      col?: ColProps;
-      after?: ReactNode | ((ctx: UseFormReturn<TFieldValues, TContext>) => ReactNode);
-    };
-  };
+  controllers:
+    | Controllers<TFieldValues, TContext>
+    | ((context: UseFormReturn<TFieldValues, TContext>) => Controllers<TFieldValues, TContext>);
 };
