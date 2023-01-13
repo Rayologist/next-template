8fa62672ca88579b5f95602a5b2dafc76e26df47
Bowei Chen <bwchen.dev@gmail.com>
2023-01-14 01:07:00 +0800

    Remove memo and displayName

--- src/components/Table/components/GlobalFilter.tsx
+++ src/components/Table/components/GlobalFilter.tsx
@@ -2,11 +2,9 @@ import { memo } from 'react';
 import { TextInput, TextInputProps } from '@mantine/core';
 import { IconSearch } from '@tabler/icons';
 
-const GlobalFilter = memo((props: TextInputProps) => {
+const GlobalFilter = (props: TextInputProps) => {
   const Icon = memo(() => <IconSearch size={14} stroke={1.5} />);
   return <TextInput placeholder="Search" icon={<Icon />} {...props} />;
-});
-
-GlobalFilter.displayName = 'GlobalFilter';
+};
 
 export default GlobalFilter;
